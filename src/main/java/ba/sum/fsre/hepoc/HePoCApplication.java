48b07587dd1ae72f01e73936e4572c57a1584aea
vladosantic <vladosantic@hotmail.com>
2024-08-17 16:27:09 +0200

    App create

---
+++ src/main/java/ba/sum/fsre/hepoc/HePoCApplication.java
@@ -0,0 +1,13 @@
+package ba.sum.fsre.hepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HePoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HePoCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/sum/fsre/hepoc/HePoCApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.sum.fsre.hepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HePoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

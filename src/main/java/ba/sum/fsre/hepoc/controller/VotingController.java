b9de3c471de48f356329ef0f3efc4b42db9edd42
vladosantic <vladosantic@hotmail.com>
2024-08-20 22:08:51 +0200

    Implement logic for proper rendering of Voting page

--- src/main/java/ba/sum/fsre/hepoc/controller/VotingController.java
+++ src/main/java/ba/sum/fsre/hepoc/controller/VotingController.java
@@ -1,15 +1,19 @@
 package ba.sum.fsre.hepoc.controller;
 
 import ba.sum.fsre.hepoc.entity.Candidate;
+import ba.sum.fsre.hepoc.entity.Citizen;
 import ba.sum.fsre.hepoc.entity.Election;
 import ba.sum.fsre.hepoc.service.CandidateService;
+import ba.sum.fsre.hepoc.service.CitizenService;
 import ba.sum.fsre.hepoc.service.ElectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -20,16 +24,25 @@ public class VotingController {
     @Autowired
     private final CandidateService candidateService;
 
-    public VotingController(ElectionService electionService, CandidateService candidateService) {
+    @Autowired
+    private final CitizenService citizenService;
+
+    public VotingController(ElectionService electionService, CandidateService candidateService, CitizenService citizenService) {
         this.electionService = electionService;
         this.candidateService = candidateService;
+        this.citizenService = citizenService;
     }
 
     @GetMapping("/voting/{id}")
-    public String showVotingPage(@PathVariable("id") Integer electionId, Model model) {
+    public String showVotingPage(@PathVariable("id") Integer electionId, Model model, Principal principal) {
+        String jmbg = principal.getName();
+        Citizen citizen = citizenService.findByJmbg(jmbg)
+                .orElseThrow(() -> new UsernameNotFoundException("Citizen not found with JMBG: " + jmbg));
+        model.addAttribute("citizen", citizen);
         Election election = electionService.getElectionById(electionId);
         List<Candidate> candidates = candidateService.findAllFromElection(election);
 
+        model.addAttribute("citizen", citizen);
         model.addAttribute("election", election);
         model.addAttribute("candidates", candidates);
 

65baf1f4f20ea0fe6857eed092abb010a75e7a11
vladosantic <vladosantic@hotmail.com>
2024-08-24 17:27:38 +0200

    Refactor CitizenDetails to ensure correct role mapping

--- src/main/java/ba/sum/fsre/hepoc/security/CitizenDetails.java
+++ src/main/java/ba/sum/fsre/hepoc/security/CitizenDetails.java
@@ -1,12 +1,16 @@
 package ba.sum.fsre.hepoc.security;
 
 import ba.sum.fsre.hepoc.entity.Citizen;
+import ba.sum.fsre.hepoc.entity.Role;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CitizenDetails implements UserDetails {
 
@@ -18,7 +22,14 @@ public class CitizenDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(() -> "ROLE_CITIZEN");
+        return mapRolesToAuthorities(citizen.getRoles());
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return mapRoles;
     }
 
     @Override
